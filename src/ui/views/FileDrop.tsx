85c1067a448232013273805d15769b05cac556ea
gmuditdce <gmuditdce@gmail.com>
2020-10-12 20:50:23 +0530

    Added files

---
+++ src/ui/views/FileDrop.tsx
@@ -0,0 +1,56 @@
+import * as React               from 'react';
+import { Container, Grid }      from '@material-ui/core';
+import { makeStyles }           from '@material-ui/core/styles';
+import      Header              from '../components/Header'
+import      Sidebar             from '../components/SideBar'
+import WebIFrameView            from '../components/WebIFrameView'
+import { useState, useEffect }  from 'react';
+import Loader                   from '../components/Loader';
+
+/** Main view of the application to display all the targeted use cases */
+const useStyles = makeStyles((theme) => ({
+     root:       {
+         flexGrow:       1, 
+     },
+     fullWidth:{
+         maxWidth:       '100%'
+     },
+     container:  {
+         display:        'grid',
+         gridGap:        theme.spacing(2),
+     },
+    gridItemRight:{
+    },
+    gridItemLeft:{
+    }
+  }));
+
+function FileDrop (){
+    const classes = useStyles();
+    
+    const [showLoader, setShowLoader] = useState(true);
+  
+    useEffect(() => {
+      setTimeout(() => { setShowLoader(false);},500);
+    }, []); 
+
+    return(
+        <div>     
+            <Header showBack={false} ></Header>            
+            <Container className={classes.fullWidth}>              
+                <Grid container spacing={2}>
+                    <Grid item xs={3} className={classes.gridItemLeft}>
+                        <Sidebar></Sidebar>
+                    </Grid>
+                    <Grid item xs={9} className={classes.gridItemRight}>
+                    {showLoader  && <Loader/> }  
+                     <WebIFrameView url = "https://file-drop.co.uk/" />
+                    </Grid>
+                </Grid>
+
+            </Container>
+        </div>
+    )
+}
+
+export default FileDrop;
---
+++ src/ui/views/ForensicWorkbench.tsx
@@ -0,0 +1,60 @@
+import * as React               from 'react';
+import { Container, Grid }      from '@material-ui/core';
+import { makeStyles }           from '@material-ui/core/styles';
+import      Header              from '../components/Header'
+import      Sidebar             from '../components/SideBar'
+import WebIFrameView            from '../components/WebIFrameView'
+import Loader                   from '../components/Loader';
+
+const useStyles = makeStyles((theme) => ({
+     root:       {
+         flexGrow:       1, 
+     },
+     fullWidth:{
+         maxWidth:       '100%'
+     },
+     container:  {
+         display:        'grid',
+         gridGap:        theme.spacing(2),
+     },
+    gridItemRight:{
+        position:       'relative'
+    },
+    gridItemLeft:{
+
+    }
+  }));
+const { useState, useEffect } = React;
+function ForensicWorkbench (){
+    const classes = useStyles(); 
+    const [showLoader, setShowLoader] = useState(true);
+  
+    useEffect(() => {
+        let loadingTime = process.platform == 'win32'? 5000:3000;
+        setTimeout(() => {
+            setShowLoader(false);
+        },loadingTime);
+    }, []); 
+
+    console.log(process.platform);
+
+    return(
+        <div>     
+            <Header showBack={false} ></Header>            
+            <Container className={classes.fullWidth}>              
+                <Grid container spacing={2}>
+                    <Grid item xs={3} className={classes.gridItemLeft}>
+                        <Sidebar></Sidebar>
+                    </Grid>
+                    <Grid item xs={9} className={classes.gridItemRight}>
+                        {showLoader  && <Loader/> }   
+                        <WebIFrameView url = "https://forensic-workbench.com/" />
+                    </Grid>
+                </Grid>
+
+            </Container>
+        </div>
+    )
+}
+
+export default ForensicWorkbench;
