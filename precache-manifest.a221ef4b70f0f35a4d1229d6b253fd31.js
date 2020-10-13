9ede882f99f871d8a7b4b65f6e500d884c4918dc
gmuditdce <gmuditdce@gmail.com>
2020-10-13 18:26:20 +0530

    Updates

---
+++ precache-manifest.a221ef4b70f0f35a4d1229d6b253fd31.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53f15552e340298cad1804f7d4e8adf",
+    "url": "/gmuditdce/electron-app/index.html"
+  },
+  {
+    "revision": "08ee6d8842c806c1bb05",
+    "url": "/gmuditdce/electron-app/static/css/main.5b823f33.chunk.css"
+  },
+  {
+    "revision": "7855a47a64ca4abb6a7f",
+    "url": "/gmuditdce/electron-app/static/js/2.5865fc53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gmuditdce/electron-app/static/js/2.5865fc53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ee6d8842c806c1bb05",
+    "url": "/gmuditdce/electron-app/static/js/main.8161f7bd.chunk.js"
+  },
+  {
+    "revision": "e05d6b1529f8bad4d129",
+    "url": "/gmuditdce/electron-app/static/js/runtime-main.11ecc595.js"
+  },
+  {
+    "revision": "c98ae9a708855f3debba72b0caec0f21",
+    "url": "/gmuditdce/electron-app/static/media/better-kubernetes.c98ae9a7.svg"
+  },
+  {
+    "revision": "7a0f87caa1f0e402af66dc52f537f07d",
+    "url": "/gmuditdce/electron-app/static/media/body-background.7a0f87ca.png"
+  },
+  {
+    "revision": "39680a8c4bf706e5a22c211affc8db5f",
+    "url": "/gmuditdce/electron-app/static/media/built-in-prometheus-stats.39680a8c.svg"
+  },
+  {
+    "revision": "deb1a686b92aa05137e3f4e87126003f",
+    "url": "/gmuditdce/electron-app/static/media/context-aware-terminal.deb1a686.svg"
+  },
+  {
+    "revision": "82f026e24b5490196a3c079f3d49d061",
+    "url": "/gmuditdce/electron-app/static/media/header-background.82f026e2.png"
+  },
+  {
+    "revision": "780695210c1bc7ce2562588ffbcb0953",
+    "url": "/gmuditdce/electron-app/static/media/header-lens.78069521.png"
+  },
+  {
+    "revision": "4d89dba8a292d974769ba2214b9d2128",
+    "url": "/gmuditdce/electron-app/static/media/logo.4d89dba8.svg"
+  },
+  {
+    "revision": "8f5e7e8c88bcae2a0cef17f2c37da8d9",
+    "url": "/gmuditdce/electron-app/static/media/multi-cluster-management.8f5e7e8c.svg"
+  },
+  {
+    "revision": "66588881babf25919a1ff7fa4125062e",
+    "url": "/gmuditdce/electron-app/static/media/workspaces.66588881.svg"
+  }
+]);
